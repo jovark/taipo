96e61e699f4f7905944d833d40a71871e39cdeeb
joonatan <joonatan@mydesktop.localdomain>
2022-05-27 19:52:09 +0300

    fix: removed old readme

--- frontend/components/Text.tsx
+++ frontend/components/Text.tsx
@@ -3,42 +3,43 @@ import useKeyPress from '../hooks/useKeyPress';
 import textStyles from '../styles/Text.module.css';
 
 const Text = ({ words }: PropsWithRef<{ words: string }>) => {
-  const [typedChars, setTypedChars] = useState('');
-  const [currentChar, setCurrentChar] = useState(words.charAt(0));
-  const [chars, setChars] = useState(words.substring(1));
+    const [typedChars, setTypedChars] = useState('');
+    const [currentChar, setCurrentChar] = useState(words.charAt(0));
+    const [chars, setChars] = useState(words.substring(1));
 
-  useKeyPress((key: string) => {
-    let updatedTypedChars = typedChars;
-    let updatedChars = chars;
+    useKeyPress((key: string) => {
+        let updatedTypedChars = typedChars;
+        let updatedChars = chars;
 
-    if (key === currentChar) {
-      updatedTypedChars += currentChar;
-      setTypedChars(updatedTypedChars);
+        if (key === currentChar) {
+            updatedTypedChars += currentChar;
+            setTypedChars(updatedTypedChars);
 
-      setCurrentChar(chars.charAt(0));
+            setCurrentChar(chars.charAt(0));
 
-      updatedChars = chars.substring(1);
-      setChars(updatedChars);
-    } else if (key === 'Backspace') {
-      setCurrentChar(typedChars.charAt(typedChars.length - 1));
+            updatedChars = chars.substring(1);
+            setChars(updatedChars);
+        } else if (key === 'Backspace') {
+            setCurrentChar(typedChars.charAt(typedChars.length - 1));
 
-      updatedChars = currentChar + chars;
-      setChars(updatedChars);
+            updatedChars = currentChar + chars;
+            setChars(updatedChars);
 
-      updatedTypedChars = typedChars.slice(0, -1);
-      setTypedChars(updatedTypedChars);
-    } else {
-      // :TODO add visual indicator for making mistakes
-    }
-  });
+            updatedTypedChars = typedChars.slice(0, -1);
+            setTypedChars(updatedTypedChars);
+        } else {
+            // :TODO add visual indicator for making mistakes
+        }
+    });
 
-  return (
-    <p className={textStyles.text}>
-      <span className={textStyles.typed}>{typedChars}</span>
-      <span className={textStyles.current}>{currentChar}</span>
-      <span className={textStyles.coming}>{chars.substring(0, 250)}</span>
-    </p>
-  );
+    return (
+        <p className={textStyles.text}>
+            {/* 67 */}
+            <span className={textStyles.typed}>{typedChars}</span>
+            <span className={textStyles.current}>{currentChar}</span>
+            <span className={textStyles.coming}>{chars.substring(0, 300)}</span>
+        </p>
+    );
 };
 
 export default Text;
